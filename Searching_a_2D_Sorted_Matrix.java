a6f57ffa7093dce6e321392b94ef33397a20c982
Youchen <youchen.r@gmail.com>
2015-05-11 23:48:52 -0400

    Round 2

--- Searching_a_2D_Sorted_Matrix.java
+++ Searching_a_2D_Sorted_Matrix.java
@@ -1,4 +1,43 @@
 /**
+ * Round 2
+ */
+public class Solution {
+    /**
+     * @param matrix: A list of lists of integers
+     * @param: A number you want to search in the matrix
+     * @return: An integer indicate the occurrence of target in the given matrix
+     */
+    public int searchMatrix(int[][] matrix, int target) {
+        // write your code here
+        //2015-05-11 11:10:46 - 2015-05-11 11:21:04 (11 min)
+        if(matrix == null || matrix.length == 0)
+            return 0;
+        
+        int count = 0, row = matrix.length, col = matrix[0].length;
+        int sRow = row - 1, sCol = 0;
+        while(sRow >= 0 && sCol <= col - 1){
+            if(matrix[sRow][sCol] >= target){//move up
+                if(matrix[sRow][sCol] == target)
+                    count++;
+                    sRow--;
+            }
+            else{// if(matrix[sRow][sCol] < target){//move right
+                    sCol++;
+            }
+        }
+        return count;
+    }
+}
+
+
+
+
+
+
+
+
+
+/**
  * Searching a 2D Sorted Matrix
  *
  * Write an efficient algorithm that searches for a value in an n x m table (two-dimensional array).
