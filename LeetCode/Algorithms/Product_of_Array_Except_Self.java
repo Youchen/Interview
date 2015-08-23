10aacf08ef1187456b7f9a06522621942c9f9ef3
Youchen <youchen.r@gmail.com>
2015-08-22 20:01:39 -0400

    Round 2
    Round 2

--- LeetCode/Algorithms/Product_of_Array_Except_Self.java
+++ LeetCode/Algorithms/Product_of_Array_Except_Self.java
@@ -2,6 +2,43 @@
  * O(n)
  * O(1)
  * 
+ * Review
+ *      Just remember a little bit and induction on paper when interview.
+ *      You will find the rule and trick.
+ * 
+ * This is called "polygenelubricants" (多基因润滑剂?) Algorithm.
+ * 
+ * Reference:
+ *      http://stackoverflow.com/questions/2680548/given-an-array-of-numbers-return-array-of-products-of-all-other-numbers-no-div
+ */
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len];
+        
+        res[0] = 1;
+        for(int i = 1; i < len; i++)
+            res[i] = res[i - 1] * nums[i - 1];
+        
+        int right = 1;
+        for(int i = len - 1; i >= 0; i--){
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
+
+
+
+
+
+
+
+/**
+ * O(n)
+ * O(1)
+ * 
  * This is called "polygenelubricants" (多基因润滑剂?) Algorithm.
  * 
  * Reference:
